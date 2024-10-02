714cfda3977704faca484f3dee20cd8ddeef172b
alperen <alperen61@gmail.com>
2024-10-02 15:43:05 +0300

    Removed unnecessary conditional rendering for empty todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,17 @@
+import React from "react";
 import { TodoItem, TodoForm } from "./";
 import useTodos from "../hooks/useTodos";
 
 export default function TodoList() {
   const { todos } = useTodos();
 
+  const todoList = todos || [];
+
   return (
     <div className="todo-container">
       <TodoForm />
       <ul className="todo-list">
-        {todos.map((todo) => (
+        {todoList.map((todo) => (
           <TodoItem key={todo.id} todo={todo} />
         ))}
       </ul>
