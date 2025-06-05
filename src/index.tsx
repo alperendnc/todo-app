ec4f2ef06791dd3be3ee032f6942455742d614b6
alperen <alperen61@gmail.com>
2025-06-05 18:39:28 +0300

    update GH Pages deployment config

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { TodosProvider } from "./contexts/TodoContext.js";
 import { AuthProvider } from "./contexts/UseAuth";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/todo-app">
+    <HashRouter>
       <AuthProvider>
         {" "}
         <TodosProvider>
           <App />
         </TodosProvider>
       </AuthProvider>{" "}
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
